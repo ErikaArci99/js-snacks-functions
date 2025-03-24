928184524633ee70e7e4f6a2cb86d21fe92272e9
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-24 15:56:02 +0100

    [UPD] finito es 3 e corretti gli errori

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,21 +1,22 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const letterA = 'A';
 
 // Dichiara la funzione qui.
 
 function filterByLetter(names, letter) {
-    for (let i = 0; i < names.lenght; i++) {
-        if (names[i][0] === letters) {
+    const result = [];
+    for (let i = 0; i < names.length; i++) {
+        if (names[i][0] === letter) {
             result.push(names[i]);
         }
     }
+    return result;
 }
-return result;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filterByLetter(names, letterA));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
