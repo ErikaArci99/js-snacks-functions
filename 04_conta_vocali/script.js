680ca39982c7658bba6f732889bdd7ffb6788bea
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-24 16:13:40 +0100

    [UPD] aggiunta funzione e 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,17 @@ const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
 
+function contaVocali (word, vocali){
+    let count = 0;
+    for (i=0; i < word.length; i++){
+        if (vocali.includes (word[i])){
+            count ++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
